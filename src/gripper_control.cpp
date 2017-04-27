3d03efd2f6044bbb975f0d810a64fccdfdf41194
axn337 <axn337@case.edu>
2017-04-27 07:13:31 -0400

    added gripperControl and Polaris transformation

---
+++ src/gripper_control.cpp
@@ -0,0 +1,49 @@
+#include <ros/ros.h> 
+#include <std_msgs/Float64.h> 
+#include "baxter_core_msgs/EndEffectorCommand.h"
+#include "baxter_core_msgs/EndEffectorState.h"
+
+//https://groups.google.com/a/rethinkrobotics.com/forum/#!topic/brr-users/ZlfK-fqRfuw
+
+
+ros::Publisher gripper_publisher;
+
+
+void polarisCB(const geometry_msgs::PoseArray& targets) 
+{ 
+	//TODO
+	baxter_core_msgs::EndEffectorCommand> controlled_grip;
+	double distance;
+
+	distance=sqrt(pow(targets.pose[0].position.x-targets.pose[1].position.x)+pow(targets.pose[0].position.y-targets.pose[1].position.y)+pow(targets.pose[0].position.z-targets.pose[1].position.z));
+
+
+	controlled_grip.command= controlled_grip.CMD_GO
+	controlled_grip.id= 65538; //65664  echo the topic 'robot/end_effector/' + name + '/state'
+	controlled_grip.args.position=distance;
+	controlled_grip.sequence = 3;//1
+	controlled_grip.sender = "/grasp_node";
+
+
+	gripper_publisher.publish(controlled_grip);
+
+}
+int main(int argc, char **argv){
+	ros::init(argc,argv,"polaris_transformation");
+	ros::NodeHandle n; 
+
+	ros::Subscriber polaris_subscriber= n.subscribe("targets",1,polarisCB); 
+	ros::Publisher gripper_pub = nh.advertise<baxter_core_msgs/EndEffectorCommand>("/robot/end_effector/left_gripper/command", 1);
+	gripper_publisher.publish(right_pose); 
+	
+	ros::Rate naptime(1);
+
+
+	naptime.sleep();
+
+
+	ros::spin(); 
+	return 0;
+}
+	 
+	
---
+++ src/polaris_transformation.cpp
@@ -0,0 +1,69 @@
+#include <ros/ros.h> 
+#include <std_msgs/Float64.h> 
+#include <geometry_msgs/PoseArray.h>
+#include <cmath>
+
+
+
+ros::Publisher right_finger_publisher;
+ros::Publisher left_finger_publisher;
+
+
+void polarisCB(const geometry_msgs::PoseArray& targets) 
+{ 
+	//TODO
+	geometry_msgs::PoseArray right_pose;
+	geometry_msgs::PoseArray left_pose;
+
+	right_pose.header.stamp = targets.header.stamp;
+	right_pose.header.frame_id = "/base";
+
+	right_pose.pose.position.x=targets.pose[0].position.x;
+	right_pose.pose.position.y=targets.pose[0].position.y;
+	right_pose.pose.position.z=-targets.pose[0].position.z;
+
+	right_pose.pose.orientation.x=targets.pose[0].orientation.x;
+	right_pose.pose.orientation.y=-targets.pose[0].orientation.y;
+	right_pose.pose.orientation.z=-targets.pose[0].orientation.z;
+	right_pose.pose.orientation.w=-targets.pose[0].orientation.w;
+
+
+
+	left_pose.header.stamp = targets.header.stamp;
+	left_pose.header.frame_id = "/base";
+
+	left_pose.pose.position.x=targets.pose[1].position.x;
+	left_pose.pose.position.y=targets.pose[1].position.y;
+	left_pose.pose.position.z=-targets.pose[1].position.z;
+
+	left_pose.pose.orientation.x=targets.pose[1].orientation.x;
+	left_pose.pose.orientation.y=-targets.pose[1].orientation.y;
+	left_pose.pose.orientation.z=-targets.pose[1].orientation.z;
+	left_pose.pose.orientation.w=-targets.pose[1].orientation.w;
+
+
+	right_finger_publisher.publish(right_pose);
+	right_finger_publisher.publish(right_pose);
+
+}
+int main(int argc, char **argv){
+	ros::init(argc,argv,"polaris_transformation");
+	ros::NodeHandle n; 
+
+	ros::Subscriber polaris_subscriber= n.subscribe("targets",1,polarisCB); 
+	ros::Publisher right_finger_pub = nh.advertise<geometry_msgs::PoseArray>("right_finger_wrt_polaris", 1);
+	right_finger_pub.publish(right_pose); 
+	ros::Publisher left_finger_pub = nh.advertise<geometry_msgs::PoseArray>("left_finger_wrt_polaris", 1);
+	left_finger_pub.publish(left_pose); 
+	
+	ros::Rate naptime(1);
+
+
+	naptime.sleep();
+
+
+	ros::spin(); 
+	return 0;
+}
+	 
+	
